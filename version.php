12c807b652fc8615312d7721c579a17f6ff88a02
Andrew Rowatt <a.j.rowatt@massey.ac.nz>
2025-04-28 08:48:34 +1200

    Correctly set version to match current MU configuration

--- version.php
+++ version.php
@@ -28,5 +28,5 @@ $plugin->component    = 'local_faultreporting';
 $plugin->release      = '1.0.4';
 $plugin->version      = 2025011104;
 $plugin->requires     = 2024100700;
-$plugin->supported    = [405, 405];
+$plugin->supported    = [401, 405];
 $plugin->maturity     = MATURITY_STABLE;
